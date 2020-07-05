9a0b721fb59de8647db22e28d8ff73cd0b95a2de
tlgadrkn <tolgadurukan16@gmail.com>
2020-07-05 22:25:58 +0300

    added ejs view engine with basic content + added new pages

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
-});
+app.set("view engine", "ejs");
 
-app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
-});
+app.get("/", (req, res) => res.render("index"));
 
-app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
-});
+app.get("/about", (req, res) => res.render("about"));
+
+app.get("/blogs/create", (req, res) => {
+  res.render("createNewBlog");
+})
+
+app.use((req, res) => res.status(404).render("404"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
