5b3bf8b6c927c1149360051ee0944461487650c4
Tolga Durukan <tolgadurukan16@gmail.com>
2020-07-18 22:40:17 +0300

    added static file serving

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 
+// middleware & static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
   const blogs = [
     {
       title: "How I got into Web Development",
-      body: "blbalbalblalbalblablalblablb"
+      body: "blbalbalblalbalblablalblablb",
     },
     {
       title: "My Life as a software developers",
-      body: "blbalbalblalbalblablalblablb"
+      body: "blbalbalblalbalblablalblablb",
     },
     {
       title: "How to monetize Youtube",
-      body: "blbalbalblalbalblablalblablb"
-    }
+      body: "blbalbalblalbalblablalblablb",
+    },
   ];
-  res.render("index", { title: "| Home", blogs});
+  res.render("index", { title: "| Home", blogs });
 });
-
 app.get("/about", (req, res) => res.render("about", { title: "| About" }));
 
 app.get("/blogs/create", (req, res) => {
